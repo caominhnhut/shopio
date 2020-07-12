200a5acf08542e7395096f40c7a862730b446d83
Nhut.Nguyen <Nhut.Nguyen@topicus.nl>
2020-07-12 13:17:58 +0700

    Hello

--- shopio/sio-rest/src/main/java/com/gls/sio/rest/config/AuthenticationFilter.java
+++ shopio/sio-rest/src/main/java/com/gls/sio/rest/config/AuthenticationFilter.java
@@ -20,7 +20,7 @@ import io.jsonwebtoken.ExpiredJwtException;
 
 public class AuthenticationFilter extends OncePerRequestFilter
 {
-	//TODO: xxx
+	//TODO: yyyy
 	private UserDetailsService userDetailsService;
 
 	private TokenHelper tokenHelper;
