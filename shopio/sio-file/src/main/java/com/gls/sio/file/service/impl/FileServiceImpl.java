6f8798fdcab98170b7734dfab82a4407c18bef08
Nhut.Nguyen <Nhut.Nguyen@topicus.nl>
2020-07-13 00:36:25 +0700

    BE_0001: Added configuration for saving product images

--- shopio/sio-file/src/main/java/com/gls/sio/file/service/impl/FileServiceImpl.java
+++ shopio/sio-file/src/main/java/com/gls/sio/file/service/impl/FileServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.Calendar;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,13 +21,18 @@ import com.gls.sio.persistent.entity.FileEntity;
 import com.gls.sio.persistent.repository.FileRepository;
 
 @Service
+@PropertySources({ @PropertySource("classpath:file.properties") })
 public class FileServiceImpl implements FileService {
 	
-	private final static String DOWN_LOAD_URI = "/product-images/";
-	private final static String PRODUCT_IMAGES_DIRECTORY = "D:/Individual/eclipse-workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/sio-console/product-images/%s";
+	private final static String DOWN_LOAD_URI = "product.image.uri";
+	private final static String PRODUCT_IMAGES_DIRECTORY = "product.image.filepath";
+	
 	private final static String INVALID_FILENAME_MESSAGE = "Filename contains invalid path sequence: [%s]";
 	private final static String ERROR_SAVING_MESSAGE = "Error when saving file: [%s]";
 
+	@Autowired
+	private Environment environment;
+	
 	@Autowired
 	private FileRepository fileRepository;
 
@@ -47,12 +55,12 @@ public class FileServiceImpl implements FileService {
 
 			FileEntity fileEntity = new FileEntity(uniqueName, file.getContentType(), file.getBytes());
 
-			String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path(DOWN_LOAD_URI)
+			String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path(environment.getProperty(DOWN_LOAD_URI))
 					.path(fileEntity.getFilename()).toUriString();
 
 			fileEntity.setFileDownloadUri(fileDownloadUri);
 			
-			File image = new File(String.format(PRODUCT_IMAGES_DIRECTORY, fileEntity.getFilename()));
+			File image = new File(String.format(environment.getProperty(PRODUCT_IMAGES_DIRECTORY), fileEntity.getFilename()));
 			FileUtils.writeByteArrayToFile(image, fileEntity.getData());
 
 			return fileEntity;
